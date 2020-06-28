735d200966a7977e48c71e30cc92f7b64ed1ccac
Alvaro Zorzini <azorzini@avioconsulting.com>
2020-06-28 17:35:29 -0300

    fix: viewport in all devices

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,16 @@ import Banner from "./components/Banner";
 import Features from "./components/Features";
 import About from "./components/About";
 import Services from "./components/Services";
-import styled from "styled-components";
-
-const StyledDiv = styled.div`
-  min-width: fit-content;
-`;
 
 const App = () => {
   return (
-    <StyledDiv className="App" data-spy="scroll" data-target="navigationbar" data-offset="200">
+    <div className="App" data-spy="scroll" data-target="navigationbar" data-offset="200">
       <NavigationBar />
       <Banner />
       <Features />
       <About />
       <Services />
-    </StyledDiv>
+    </div>
   );
 }
 
