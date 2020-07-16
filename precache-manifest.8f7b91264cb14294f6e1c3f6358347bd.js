70db0a019fd6dc9efa594e1da340947643e72ad9
Alvaro Zorzini <azorzini@avioconsultig.com>
2020-07-16 15:25:20 -0300

    Updates

---
+++ precache-manifest.8f7b91264cb14294f6e1c3f6358347bd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527423446a9253c80cc41a70e7e97865",
+    "url": "/intertrader-site/index.html"
+  },
+  {
+    "revision": "0c5aa6a86cc8acec459b",
+    "url": "/intertrader-site/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a42ead5696f491a4c752",
+    "url": "/intertrader-site/static/css/main.f4f4af13.chunk.css"
+  },
+  {
+    "revision": "0c5aa6a86cc8acec459b",
+    "url": "/intertrader-site/static/js/2.09f7dc19.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/intertrader-site/static/js/2.09f7dc19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a42ead5696f491a4c752",
+    "url": "/intertrader-site/static/js/main.55589bc5.chunk.js"
+  },
+  {
+    "revision": "fc53e368c9beeac5e372",
+    "url": "/intertrader-site/static/js/runtime-main.b1802571.js"
+  },
+  {
+    "revision": "5b6dfdf557a202295a30be65e5dfa3e7",
+    "url": "/intertrader-site/static/media/about.5b6dfdf5.jpg"
+  },
+  {
+    "revision": "70a7b4dd40035f52546e384eb069a4ba",
+    "url": "/intertrader-site/static/media/intro-bg.70a7b4dd.jpg"
+  }
+]);
